d13bf5efe8bce6f232151ad84363ff383ef91870
rperlop <rubperezlopez@gmail.com>
2023-04-24 14:48:26 +0200

    fix: delete unused code lines

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,6 @@
 <!-- Spinner Start -->
 <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
     <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-        <span class="sr-only">Loading...</span>
     </div>
 </div>
 <!-- Spinner End -->
@@ -48,9 +47,8 @@
 <!-- Navbar & Hero Start -->
 <div class="container-xxl position-relative p-0">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-        <a href="" class="navbar-brand p-0">
+        <a href="{{ url('/') }}" class="navbar-brand p-0">
             <h1 class="text-primary m-0"><i class="fa fa-bowl-food me-3"></i>YourMeal</h1>
-        <!-- <img src="{{asset('img/logo.png" alt="Logo')}}"> -->
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="fa fa-bars"></span>
