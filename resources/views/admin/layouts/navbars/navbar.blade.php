d36502b2e7f6fca46281eb9192ced57e5153cd43
rperlop <rubperezlopez@gmail.com>
2023-05-31 19:18:11 +0200

    fix: deleted possible burger responsive button in panel admin

--- resources/views/admin/layouts/navbars/navbar.blade.php
+++ resources/views/admin/layouts/navbars/navbar.blade.php
@@ -1,11 +1,6 @@
 <nav class="navbar navbar-expand-lg ">
     <div class="container-fluid">
         <span class="navbar-brand"> {{ $navName }} </span>
-        <button href="" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar burger-lines"></span>
-            <span class="navbar-toggler-bar burger-lines"></span>
-            <span class="navbar-toggler-bar burger-lines"></span>
-        </button>
         <div class="collapse navbar-collapse justify-content-end" id="navigation">
             <ul class="nav navbar-nav mr-auto">
                 <li class="nav-item">
